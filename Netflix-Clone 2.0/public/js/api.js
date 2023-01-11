0e0bf3db6741923e29ff5848607b4dc77ba2aa98
pbcarvalho <peterson.carvalho20@gmail.com>
2023-01-10 21:15:18 -0300

    Projeto Clone - Netflix-Clone 2.0

---
+++ Netflix-Clone 2.0/public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "b8bffee1171bbc16632db1e8598e41fb";
+
+let img_url = "https://image.tmdb.org/t/p/w500/";
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
