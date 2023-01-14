25b234cbca0d5d61ff9b3992a861f4e0723e5a7f
pbcarvalho <peterson.carvalho20@gmail.com>
2023-01-13 22:40:21 -0300

    Projeto Urna Eletrônica

---
+++ Urna Eletrônica/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR(A)',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulana de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url:'cand5.jpeg', legenda:'Vereadora'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano de Vilar',
+                partido: 'DEFG',
+                fotos: [
+                    {url:'cand1.jpeg', legenda:'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO(A)',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclana',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url:'cand4.jpeg', legenda:'Prefeita'},
+                    {url:'cand2.jpeg', legenda:'Vice-Prefeito', pequeno: true},
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Fill Meidor',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url:'cand3.jpeg', legenda:'Prefeito'},
+                    {url:'cand6.jpeg', legenda:'Vice-Prefeito', pequeno: true},
+                ]
+            }
+        ]
+    }
+];
